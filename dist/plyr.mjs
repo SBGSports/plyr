1ece4a3a9f16147bf137f7bb2b431bc88d3c6ce3
Danielh112 <Daniel@sbgsportssoftware.com>
2020-07-31 10:27:45 +0100

    Update dist folder

--- dist/plyr.mjs
+++ dist/plyr.mjs
@@ -4924,6 +4924,16 @@ var Listeners = /*#__PURE__*/function () {
             // L key
             player.loop = !player.loop;
             break;
+
+          case 73:
+            // I key
+            player.trim.setTrimStart();
+            break;
+
+          case 79:
+            // O key
+            player.trim.setTrimEnd();
+            break;
         } // Escape is handle natively when in full screen
         // So we only need to worry about non native
 
@@ -8818,10 +8828,8 @@ var Trim = /*#__PURE__*/function () {
         this.setLeftThumbPosition(percentage);
       } else if (this.editing === rightThumb) {
         this.setRightThumbPosition(percentage);
-      } // Update the shaded out regions on the timeline
+      } // Show the seek thumbnail
 
-
-      this.setShadedRegions(); // Show the seek thumbnail
 
       if (this.previewThumbnailsReady) {
         var seekTime = this.player.media.duration * (percentage / 100);
@@ -8829,6 +8837,22 @@ var Trim = /*#__PURE__*/function () {
       }
 
       triggerEvent.call(this.player, this.player.media, 'trimchanging', false, this.trimTime);
+    }
+  }, {
+    key: "setTrimStart",
+    value: function setTrimStart() {
+      var time = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.player.currentTime;
+      var percentage = clamp(100 / this.player.duration * parseFloat(time), this.lowerBound, this.upperBound);
+      this.setLeftThumbPosition(percentage);
+      triggerEvent.call(this.player, this.player.media, 'trimchange', false, this.trimTime);
+    }
+  }, {
+    key: "setTrimEnd",
+    value: function setTrimEnd() {
+      var time = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.player.currentTime;
+      var percentage = clamp(100 / this.player.duration * parseFloat(time), this.lowerBound, this.upperBound);
+      this.setRightThumbPosition(percentage);
+      triggerEvent.call(this.player, this.player.media, 'trimchange', false, this.trimTime);
     }
   }, {
     key: "setLeftThumbPosition",
@@ -8852,6 +8876,7 @@ var Trim = /*#__PURE__*/function () {
 
       this.setThumbTimeStamps();
       this.setThumbAriaData();
+      this.setShadedRegions();
     }
   }, {
     key: "setRightThumbPosition",
@@ -8883,6 +8908,7 @@ var Trim = /*#__PURE__*/function () {
 
       this.setThumbTimeStamps();
       this.setThumbAriaData();
+      this.setShadedRegions();
     }
   }, {
     key: "setThumbTimeStamps",
