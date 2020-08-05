368cc8b38a96d181ec43d24c91a9b5337fdcd2b3
Danielh112 <Daniel@sbgsportssoftware.com>
2020-08-05 10:59:41 +0100

    Update dist

--- dist/plyr.polyfilled.js
+++ dist/plyr.polyfilled.js
@@ -11517,7 +11517,7 @@ typeof navigator === "object" && (function (global, factory) {
         }); // Handle media fragments end event, as event is not fired by default
 
         on.call(player, player.media, 'timeupdate seeking seeked', function () {
-          if (!player.mediaFragment.enabled || player.currentTime < player.duration) return; // Media fragments are not automatically stopped at end of playback
+          if (!player.mediaFragment.enabled || !player.duration || player.currentTime < player.duration) return; // Media fragments are not automatically stopped at end of playback
 
           player.pause(); // Manually trigger default event ended as ended event will not trigger for media fragments
 
@@ -13286,16 +13286,12 @@ typeof navigator === "object" && (function (global, factory) {
     }, {
       key: "destroy",
       value: function destroy() {
-        var _this2 = this;
-
         if (!this.enabled) return;
         var config = this.player.config;
         this.active = false; // Reset start and duration back to default values
 
         config.startTime = 0;
-        this.player.once('loadedmetadata', function () {
-          config.duration = _this2.media.duration;
-        });
+        if (this.media) config.duration = this.media.duration;
       }
     }, {
       key: "enabled",
@@ -17579,9 +17575,9 @@ typeof navigator === "object" && (function (global, factory) {
 
 
       if (this.isHTML5 && this.config.autoplay) {
-        setTimeout(function () {
+        this.once('canplay', function () {
           return silencePromise(_this.play());
-        }, 10);
+        });
       } // Seek time will be recorded (in listeners.js) so we can prevent hiding controls for a few seconds after seek
 
 
