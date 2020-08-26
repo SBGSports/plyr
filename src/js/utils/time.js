078a29b132bf893f2626f753d64a8369cf8f784c
Danielh112 <Daniel@sbgsportssoftware.com>
2020-08-26 14:33:54 +0100

    Addition of match to video time function and limit match minutes to the period time e.g. 48+2:01 = 45+2:01

--- src/js/utils/time.js
+++ src/js/utils/time.js
@@ -36,13 +36,16 @@ export function formatTime(time = 0, displayHours = false, inverted = false) {
   return `${inverted && time > 0 ? '-' : ''}${hours}${format(mins)}:${format(secs)}`;
 }
 
-export function matchTime(time, syncPoints) {
+export function videoToMatchTime(time, syncPoints) {
   const syncPointsOrdered = syncPoints.sort((a, b) => b.time - a.time);
   const syncPoint = syncPointsOrdered.find(x => x.time <= time) || syncPointsOrdered[syncPointsOrdered.length - 1];
   const timeIntoPeriod = Math.max(time - syncPoint.time, 0);
   // Time into period
   const seconds = getSeconds(timeIntoPeriod);
-  const minutes = secondsToMinutes(syncPoint.start + timeIntoPeriod - seconds);
+  const minutes = Math.min(
+    secondsToMinutes(syncPoint.start + timeIntoPeriod - seconds),
+    secondsToMinutes(syncPoint.start + syncPoint.duration),
+  );
   const minutesIntoPeriod = secondsToMinutes(timeIntoPeriod - seconds);
   // Added time into period
   const addedTime = minutesIntoPeriod >= secondsToMinutes(syncPoint.duration);
@@ -51,4 +54,28 @@ export function matchTime(time, syncPoints) {
   const format = value => `${value.toString().length <= 1 ? '0' : ''}${value}`;
 
   return `${format(minutes)}${addedTime ? `+${format(addedMinutes)}` : ''}:${format(seconds)}`;
+}
+
+export function matchToVideoTime(time, syncPoints) {
+  // Match Time
+  const syncPointsOrdered = syncPoints.sort((a, b) => a.time - b.time);
+  const matchTime = time.match(/(^[0-9]{1,3})\+?([0-9]{1,2})?:([0-9]{1,2})/);
+  const minutes = Number(matchTime[1] || 0);
+  let addedMinutes = Number(matchTime[2] || -1);
+  const seconds = Number(matchTime[3] || 0);
+  const hasAddedMinutes = addedMinutes >= 0;
+  addedMinutes = Math.max(addedMinutes, 0);
+
+  // Store match time in seconds
+  const matchTimeInSeconds = minutes * 60 + addedMinutes * 60 + seconds;
+  // Match Period Index
+  let index = 0;
+  syncPointsOrdered.forEach(x => {
+    if (x.start + x.duration <= matchTimeInSeconds) index += 1;
+  });
+
+  // Offset index if added time
+  const matchPeriod = syncPointsOrdered[hasAddedMinutes ? index - 1 : Math.min(index, syncPoints.length - 1)];
+
+  return matchTimeInSeconds + matchPeriod.time - matchPeriod.start;
 }
